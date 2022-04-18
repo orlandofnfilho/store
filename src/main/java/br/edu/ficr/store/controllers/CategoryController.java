b393d0e051a2364b026c0911ab7fe3951f6c560d
Orlando Ferreira <orlandofnfilho@gmail.com>
2022-04-18 10:29:55 -0300

    update

--- src/main/java/br/edu/ficr/store/controllers/CategoryController.java
+++ src/main/java/br/edu/ficr/store/controllers/CategoryController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ficr.store.entities.Category;
 import br.edu.ficr.store.services.CategoryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,32 +28,57 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 
-	@PostMapping("/categories")
+	@PostMapping("/categorys")
 	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation(value = "Add a new category")
+	@ApiResponses( value = {
+		@ApiResponse(code = 201, message = "New category created successful"),
+		@ApiResponse(code = 500, message = "Error adding a category")
+	})
 	public Category addCategory(@RequestBody Category cat) {
 		return categoryService.addCategory(cat);
 	}
 
-	@GetMapping("/categories")
+	@GetMapping("/categorys")
 	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "Listing all categorys")
+	@ApiResponses( value = {
+		@ApiResponse(code = 200, message = "All categories listed successfully"),
+		@ApiResponse(code = 500, message = "Error listing all categories")
+	})
 	public List<Category> findAll() {
 		return categoryService.findAll();
 	}
 
-	@GetMapping("/categories/{id}")
+	@GetMapping("/categorys/{id}")
 	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "Finding category by Id")
+	@ApiResponses( value = {
+		@ApiResponse(code = 200, message = "Category successfully found"),
+		@ApiResponse(code = 404, message = "Category not found")
+	})
 	public ResponseEntity<Category> findById(@PathVariable Long id) {
 		return categoryService.findById(id);
 	}
 
-	@PutMapping("/categories/{id}")
+	@PutMapping("/categorys/{id}")
 	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "Updating a category")
+	@ApiResponses( value = {
+		@ApiResponse(code = 200, message = "Category successfully updated"),
+		@ApiResponse(code = 404, message = "Error updating the category")
+	})
 	public ResponseEntity<Category> findById(@RequestBody Category category, @PathVariable Long id) {
 		return categoryService.updateCategoryById(category, id);
 	}
 
-	@DeleteMapping("/categories/{id}")
+	@DeleteMapping("/categorys/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@ApiOperation(value = "Deleting a category")
+	@ApiResponses( value = {
+		@ApiResponse(code = 204, message = "Category successfully deleted"),
+		@ApiResponse(code = 404, message = "Category not found")
+	})
 	public ResponseEntity<Object> deleteById(@PathVariable Long id) {
 		return categoryService.deleteCategoryById(id);
 	}
--- src/main/java/br/edu/ficr/store/controllers/SupplierController.java
+++ src/main/java/br/edu/ficr/store/controllers/SupplierController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ficr.store.entities.Supplier;
 import br.edu.ficr.store.services.SupplierService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -27,30 +30,55 @@ public class SupplierController {
 	
 	@PostMapping("/suppliers")
 	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation(value = "Add a new supplier")
+	@ApiResponses( value = {
+		@ApiResponse(code = 201, message = "New supplier created successful"),
+		@ApiResponse(code = 500, message = "Error adding supplier")
+	})
 	public Supplier addSupplier(@RequestBody Supplier supplier) {
 		return supplierService.addSupplier(supplier);
 	}
 	
 	@GetMapping("/suppliers")
 	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "Listing all suppliers")
+	@ApiResponses( value = {
+		@ApiResponse(code = 200, message = "All suppliers listed successfully"),
+		@ApiResponse(code = 500, message = "Error listing all suppliers")
+	})
 	public List<Supplier> findAll(){
 		return supplierService.findAll();
 	}
 	
 	@GetMapping("/suppliers/{id}")
 	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "Finding supplier by Id")
+	@ApiResponses( value = {
+		@ApiResponse(code = 200, message = "Supplier successfully found"),
+		@ApiResponse(code = 404, message = "Supplier not found")
+	})
 	public ResponseEntity<Supplier> findById(@PathVariable Long id){
 		return supplierService.findById(id);
 	}
 	
 	@PutMapping("/suppliers/{id}")
 	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "Updating a supplier")
+	@ApiResponses( value = {
+		@ApiResponse(code = 200, message = "Supplier successfully updated"),
+		@ApiResponse(code = 404, message = "Error updating the supplier")
+	})
 	public ResponseEntity<Supplier> findById(@RequestBody Supplier supplier, @PathVariable Long id){
 		return supplierService.updateSupplierById(supplier, id);
 	}
 	
 	@DeleteMapping("/suppliers/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@ApiOperation(value = "Deleting a supplier")
+	@ApiResponses( value = {
+		@ApiResponse(code = 204, message = "Supplier successfully deleted"),
+		@ApiResponse(code = 404, message = "Supplier not found")
+	})
 	public ResponseEntity<Object> deleteById(@PathVariable Long id){
 		return supplierService.deleteSupplierById(id);
 	}
